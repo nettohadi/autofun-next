180e1f0eec96df20b8a92298c614c9481970250f
Hadi Syahbal <hadi.syahbal@gmail.com>
2021-06-13 13:14:19 +0800

    edit number of row when showing top car

--- components/shared/Cars.tsx
+++ components/shared/Cars.tsx
@@ -6,6 +6,7 @@ type ColCountType = 4 | 5;
 type Props = {cars: CarType[], initialColCount?:ColCountType};
 
 export default function Cars({cars, initialColCount = 5}: Props) {
+    console.log({initialColCount});
     return (
         <div className="p-7 overflow-hidden" style={{minHeight: 450}}>
             <div className={`grid grid-cols-2 md:grid-cols-4 xl:grid-cols-${initialColCount} sm:grid-cols-3 gap-10`}>
